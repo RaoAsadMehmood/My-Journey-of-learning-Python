2a6c2288891918428963577e9a857b89484d969a
RaoAsadMehmood <asadrao2489@gmail.com>
2025-02-28 22:48:33 +0500

    Modules in Python with a simple example

---
+++ Day 03/main.py
@@ -0,0 +1,10 @@
+# ---------------  Modules  || PIP  ---------------
+
+# PyJokes is a Python library for programming jokes
+# It provides a collection of one-line jokes for programmers
+# The jokes are available in different categories and languages
+# You can install it using: pip install pyjokes
+
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)
